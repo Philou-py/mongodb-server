725597888c917eb93fdc3819b3173fdc2748df53
Philippe Schoenhenz <philippe.schoenhenz@gmail.com>
2021-09-01 18:42:32 +0200

    ✨ (rest-api.ts): Create GET REST routes

--- src/rest-api.ts
+++ src/rest-api.ts
@@ -4,14 +4,20 @@ import { db } from "./app";
 const router = Router();
 
 router.get("/find", async (req, res) => {
-  const { collectionName, filter, options } = req.query as {
-    collectionName: string;
-    filter?: object;
-    options?: object;
-  };
-  console.log(collectionName, filter, options);
-  console.log(typeof collectionName, typeof filter, typeof options);
+  const { collectionName, filter, options } = req.query as any;
   const result = await db.find(collectionName, filter, options);
+  res.send(result);
+});
+
+router.get("/findOne", async (req, res) => {
+  const { collectionName, filter, options } = req.query as any;
+  const result = await db.findOne(collectionName, filter, options);
+  res.send(result);
+});
+
+router.get("/findOneWithId", async (req, res) => {
+  const { collectionName, docId } = req.query as any;
+  const result = await db.findOneWithId(collectionName, docId);
   res.send(result);
 });
 
